2078de8210a3751042454fd00c13ecfb6c2325d1
Shreyas Kapale <shreyas.kapale@gmail.com>
2019-07-25 17:43:37 +0530

    create main.js

---
+++ main.js
@@ -0,0 +1,73 @@
+
+function decide()
+{
+uname = document.getElementById("gname").value;
+uname = uname.toLowerCase(); 
+	if (uname.indexOf("as") !=-1) {
+    alert("House Targaryen");
+}
+	else if(uname.indexOf("ra") !=-1) {
+	alert("House Stark");
+}
+	else if(uname.indexOf("ha") !=-1) {
+	alert("House Martell");
+}
+	else if(uname.indexOf("ya") !=-1) {
+	alert("House Tyrell");
+}
+	else if(uname.indexOf("an") !=-1) {
+	alert("House Lannister ");
+}
+	else if(uname.indexOf("ar") !=-1) {
+	alert("House Baratheon");
+}
+	else if(uname.indexOf("ka") !=-1) {
+	alert("House Arryn");
+}
+	else if(uname.indexOf("th") !=-1) {
+	alert("House Umber");
+}
+	else if(uname.indexOf("sh") !=-1) {
+	alert("House Greyjoy");
+}
+	else if(uname.indexOf("na") !=-1) {
+	alert("House Karstark");
+}
+	else if(uname.indexOf("mi") !=-1) {
+	alert("House Baratheon");
+}
+	else if(uname.indexOf("ay") !=-1) {
+	alert("House Stark");
+}
+	else if(uname.indexOf("z") !=-1) {
+	alert("House Targaryen");
+}
+	else if(uname.indexOf("vi") !=-1) {
+	alert("House Umber");
+}
+	else if(uname.indexOf("in") !=-1) {
+	alert("House Martell");
+}
+	else if(uname.indexOf("y") !=-1) {
+	alert("House Tyrell");
+}	
+	else if(uname.indexOf("ep") !=-1) {
+	alert("House Lannister");
+}
+	else if(uname.indexOf("hn") !=-1) {
+	alert("House Targaryen");
+}
+	else if(uname.indexOf("on") !=-1) {
+	alert("House Lannister");
+}
+	else if(uname.indexOf("am") !=-1) {
+	alert("House Karstark");
+}
+	else if(uname.indexOf("sa") !=-1) {
+	alert("House Stark");
+}
+	else
+{
+	alert(" Wildling !");
+}
+}
